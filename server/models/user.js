f566dc549cbe0b05c7c8a3689f18a60536cb2660
xecarlox <jf94.uk@gmail.com.com>
2019-04-22 00:07:14 +0100

    =user sign up and login done

--- server/models/user.js
+++ server/models/user.js
@@ -1,10 +1,80 @@
 const mongoose = require("mongoose");
-
+const jwt = require("jsonwebtoken")
+const bcrypt = require("bcryptjs")
 
 const userSchema = new mongoose.Schema({
-
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+        trim: true,
+        lowercase: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        min: 6,
+        max: 15,
+        trim: true
+    },
+    // typeSubscription: {
+    //     type: String,
+    //     enum: [ "partial", "platinum" ]
+    // },
+    // admin: Boolean,
+    tokens: [
+        {
+            token: {
+                type: String,
+                required: true
+            }
+        }
+    ]
 })
 
+userSchema.statics.findByCredentials = async function(email, password) {
+    const user = await User.findOne({ email })
+
+    if(!user) throw new Error("Unable to find User")
+
+    const isMatch = await bcrypt.compare(password, user.password)
+
+    if(!isMatch) throw new Error("Password is wrong")
+
+    return user;
+}
+
+userSchema.methods.generateAuthToken = async function () {
+    const user = this;
+
+    const token = await jwt.sign( { _id: user._id.toString() }, process.env.JWT_SECRET )
+
+    user.tokens = user.tokens.concat({ token })
+
+    await user.save()
+
+    return token;
+}
+
+userSchema.methods.toJSON = function() {
+    const user = this;
+    const userObj = user.toObject()
+
+    delete userObj.password
+    delete userObj.tokens
+
+    return userObj
+}
+
+userSchema.pre("save", async function(next) {
+    const user = this;
+
+    if(user.isModified("password")) {
+        user.password = await bcrypt.hash(user.password, 8)
+    }
+
+    next()
+})
 
 const User = mongoose.model("User", userSchema);
 
--- server/routers/user.js
+++ server/routers/user.js
@@ -1,6 +1,45 @@
 const express = require('express')
-
 const router = new express.Router()
 
+const User = require("../models/user")
+
+router.get("/", async (req, res) => {
+    try {
+        const users = await User.find({})
+
+        res.send({users})
+    } catch (error) {
+        res.status(500).send()
+    }
+})
+
+router.post("/", async (req, res) => {
+    const body = req.body;
+
+    try {
+        const user = new User(body)
+
+        const token = await user.generateAuthToken()
+
+        res.send({ user, token })
+
+    } catch (error) {
+        res.status(500).send()
+    }
+})
+
+
+router.post("/login", async (req, res) => {
+    const body = req.body;
+    try {
+        const user = await User.findByCredentials(body.email, body.password)
+
+        const token = await user.generateAuthToken()
+
+        res.send({ user, token })
+    } catch (error) {
+        res.status(500).send()
+    }
+})
 
 module.exports = router
