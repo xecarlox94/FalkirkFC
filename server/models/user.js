5ce6dec093cd5f96e37ac7a3ca60d2cc2f018d31
xecarlox94 <jf94.uk@gmail.com>
2019-05-20 14:27:30 +0100

    update user.js

--- server/models/user.js
+++ server/models/user.js
@@ -1,7 +1,7 @@
-const mongoose = require("mongoose");
-const validator = require("validator")
-const jwt = require("jsonwebtoken")
-const bcrypt = require("bcryptjs")
+const mongoose = require("mongoose"); // load mongoose module
+const validator = require("validator"); // load validator module
+const jwt = require("jsonwebtoken"); // load jwt module
+const bcrypt = require("bcryptjs"); // load bcryptjs module
 
 const userSchema = new mongoose.Schema({
     firstName: {
@@ -23,8 +23,8 @@ const userSchema = new mongoose.Schema({
         unique: true,
         required: true,
         trim: true,
-        validate(value) {
-            if(validator.isEmail(value)) return true
+        validate(value) { // validates if string is email or not
+            if(validator.isEmail(value)) return true 
             else throw new Error("The value is not a email")
         }
     },
@@ -38,7 +38,7 @@ const userSchema = new mongoose.Schema({
     typeSubscription: {
         type: String,
         required: true,
-        enum: [ "partial", "platinum" ],
+        enum: [ "partial", "platinum" ], // string validation
         default: "partial"
     },
     admin: {
@@ -49,13 +49,13 @@ const userSchema = new mongoose.Schema({
     gender: {
         type: String,
         required: true,
-        enum: [ "male", "female" ]
+        enum: [ "male", "female" ] // string validation
     },
     mobilePhone: {
         type: String,
         trim: true,
         required: true,
-        validate(value) {
+        validate(value) { // validates if string is mobile from many countries
             if(validator.isMobilePhone(value, ['en-GB', 'en-US', 'pt-BR', 'pt-PT', 'pl-PL', 'it-IT', 'es-ES', 'en-AU' ])) return true
             else throw new Error("The mobile phone num is not from United Kigdom or United States")
         }
@@ -78,51 +78,51 @@ const userSchema = new mongoose.Schema({
 })
 
 userSchema.statics.findByCredentials = async function(email, password) {
-    const user = await User.findOne({ email })
+    const user = await User.findOne({ email }) // get user with specific password
 
-    if(!user) throw new Error("Unable to find User")
+    if(!user) throw new Error("Unable to find User") // if no user, throw error
 
-    const isMatch = await bcrypt.compare(password, user.password)
+    const isMatch = await bcrypt.compare(password, user.password) // check if encrypted password matches
 
-    if(!isMatch) throw new Error("Password is wrong")
+    if(!isMatch) throw new Error("Password is wrong") // if password does not match, throw error
 
     return user;
 }
 
 userSchema.methods.generateAuthToken = async function () {
-    const user = this;
+    const user = this; // assign user to this
 
-    const token = await jwt.sign( { _id: user._id.toString() }, process.env.JWT_SECRET )
+    const token = await jwt.sign( { _id: user._id.toString() }, process.env.JWT_SECRET ) // generate jwtToken with objectID and jwt secret environment variable
 
-    user.tokens = user.tokens.concat({ token })
+    user.tokens = user.tokens.concat({ token }) // add token to tokens array
 
-    await user.save()
+    await user.save() // save user
 
     return token;
 }
 
-userSchema.methods.toJSON = function() {
-    const user = this;
-    const userObj = user.toObject()
+userSchema.methods.toJSON = function() { // function when object is sent using JSON
+    const user = this; // assign user to this
+    const userObj = user.toObject() // return object without methods
 
-    delete userObj.password
-    delete userObj.tokens
+    delete userObj.password; // delete password from object
+    delete userObj.tokens; // delete tokens from object
 
     return userObj
 }
 
-userSchema.pre("save", async function(next) {
-    const user = this;
+userSchema.pre("save", async function(next) { // function when save is called
+    const user = this; // assign user to this
 
-    if(user.isModified("password")) {
-        user.password = await bcrypt.hash(user.password, 8)
+    if(user.isModified("password")) { // if password was just changed
+        user.password = await bcrypt.hash(user.password, 8) // encrypt new password
     }
 
-    next()
+    next() // call next function
 })
 
 
-const User = mongoose.model("User", userSchema);
+const User = mongoose.model("User", userSchema); // create User model based on userSchema
 
 
 module.exports = User;
