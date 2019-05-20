12be625b8aca46de3ab9391d01e948e7ebe8804c
xecarlox94 <jf94.uk@gmail.com>
2019-05-20 12:54:40 +0100

    update app.js

--- server/app.js
+++ server/app.js
@@ -1,29 +1,45 @@
-require('./db/mongoose')
-const cors = require("cors")
-const path = require("path")
-const express = require('express')
-const app = express()
+require('./db/mongoose') // executes the mongoose database file
+
+const cors = require("cors") // loads the cors dependency
+
+const path = require("path") // loads the path, a nodejs native library
+
+const express = require('express') // loads express.js, a web server application
+
+const app = express() // it intializes the express application and it stores it in th app variable
 
 
-const public_static = path.join(__dirname, "/../public");
-const routers = require("./routers/loader");
+const public_static = path.join(__dirname, "/../public"); // it stores a string location for the public folder
 
-app.use(express.json())
-app.use(cors())
-app.use(express.static(public_static))
+const routers = require("./routers/loader"); // it loads all the routers
 
-// global error handling needed
+app.use(express.json()) // middleware that sets the express application to use JSON
 
-app.use("/events", routers.eventRouter);
-app.use("/matches", routers.matchRouter);
-app.use("/matchEvents", routers.matchEventRouter);
-app.use("/news", routers.newsRouter);
-app.use("/players", routers.playerRouter);
-app.use("/teams", routers.teamRouter);
-app.use("/users", routers.userRouter);
+app.use(cors()) // it enables Cross-Origin Resource Sharing 
+
+app.use(express.static(public_static)) // makes the public folder available to clients
+
+
+
+app.use("/events", routers.eventRouter); // loads the event router and sets its global path to "/events"
+
+app.use("/matches", routers.matchRouter); // loads the matches router and sets its global path to "/matches"
+
+app.use("/matchEvents", routers.matchEventRouter); // loads the matchEvents router and sets its global path to "/matchEvents"
+
+app.use("/news", routers.newsRouter); // loads the news router and sets its global path to "/news"
+
+app.use("/players", routers.playerRouter); // loads the players router and sets its global path to "/players"
+
+app.use("/teams", routers.teamRouter); // loads the teams router and sets its global path to "/teams"
+
+app.use("/users", routers.userRouter); // loads the users router and sets its global path to "/users"
+
+
+app.get('*', (req, res) => { // it sets a wilcard for all http requests
   
-app.get('*', (req, res) => {
-    res.sendFile(path.join(public_static, '/index.html'));
+    res.sendFile(path.join(public_static, '/index.html')); // sends the index.html file, located in the public folder
+  
 });
 
-module.exports = app
+module.exports = app; // exports the express application variable
