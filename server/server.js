8582da593a04e23d6dfba4e9391c09127abb036b
xecarlox94 <jf94.uk@gmail.com>
2019-05-20 12:39:34 +0100

    add comments to server.js file

--- server/server.js
+++ server/server.js
@@ -5,22 +5,26 @@ const port = process.env.PORT
 
 
 const server = app.listen(port, () => {
-    console.log('Server is up on port ' + port)
+    console.log('Server is up on port ' + port) // logs if the server is successfully initialized
 })
 
 
-const io = socketIO(server)
+const io = socketIO(server) // initializes the websocket
 
 
-io.on("connection", (socket) => {
-    console.log("USER CONNECTED")
+io.on("connection", (socket) => { //listens to client web sockets connecting
+    
+    console.log("user connected") // logs if a websocket client is connected
 
-    socket.on("live-match", ( matchStream ) => {
-        console.log("match stream received!!!!!!!!!!!!!!!! ", matchStream)
-        socket.broadcast.emit("live-match-broadcast", matchStream )
+    socket.on("live-match", ( matchStream ) => { // listens for the "live-match" event and stores the matchStream data
+        
+        socket.broadcast.emit("live-match-broadcast", matchStream ) // broadcasts the data in 
+        
     })
 
-    socket.on("disconnect", () => {
-        console.log("user disconnected")
+    socket.on("disconnect", () => { //listens to client web sockets disconnecting
+        
+        console.log("user disconnected") // logs if a websocket client is connected
+        
     })
 })
