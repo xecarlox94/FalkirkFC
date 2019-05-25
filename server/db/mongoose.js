b8cd827ab98473e642eb2d05c539697652a65751
xecarlox <jf94.uk@gmail.com.com>
2019-05-25 15:01:36 +0100

    database error handling final

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose'); // loads mongoose module
-process.env.MONGODB_URL = "sdgsg"
+
 mongoose.connect(process.env.MONGODB_URL, { // connects to cluster
     useNewUrlParser: true,
     useCreateIndex: true,
